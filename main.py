c48a33cde4ceab545b6462250d629010c190dbdb
AFUtik <coffecraft38@gmail.com>
2023-03-19 15:26:21 +0200

    function main

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ figure, ax = plt.subplots()
 
 _x = range(-10, 10)
 
-k1, b1 = 3, 5
+k1, b1 = 3, 4
 y1 = tuple(map(lambda x: k1 * x + b1, _x))
 
-k2, b2 = 3, 2
+k2, b2 = 6, 6
 y2 = tuple(map(lambda x: k2 * x + b2, _x))
 
 def getIntersection():
@@ -25,15 +25,20 @@ def getIntersection():
   sol_y = k1 * sol_x + b1
   return sol_x, sol_y
 
+def main():
+  ax.plot(_x, y1, color="green", label='y = kx + b')
+  ax.plot(_x, y2, color="blue", label='y = kx + b')
+
+  ax.set_title("Title", fontsize=20)
+
+  ax.legend(loc="upper left")
+
+  ax.grid(color="black", alpha=0.2)
+
+  plt.show()
+
 print(getIntersection())
 
-ax.plot(_x, y1, color="green", label='y = kx + b')
-ax.plot(_x, y2, color="blue", label='y = kx + b')
-
-ax.set_title("Title", fontsize=20)
-
-ax.legend(loc="upper left")
-
-ax.grid(color="black", alpha=0.2)
-
-plt.show()
+if __name__ == "__main__":
+  main()
+  
